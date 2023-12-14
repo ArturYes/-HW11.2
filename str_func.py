9aca0cca13bb0597512cec287fc5604e8a657735
ArturYes <153022144+ArturYes@users.noreply.github.com>
2023-12-15 01:40:01 +0600

    Update str_func.py
    добавление функции другим пользователем

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def uppercase_string(text):
     return text.upper()
+
+def capitalize_words(string):
+    docstring
+    words = string.split()  # Разделяем строку на список слов
+    capitalized_words = [word.capitalize() for word in words]  # Делаем первую букву каждого слова заглавной
+    result = " ".join(capitalized_words)  # Соединяем слова обратно в строку
+    return result
