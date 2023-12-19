ccd031cd702998081c09fe8330df1aefa57b9c18
ArturYes <153022144+ArturYes@users.noreply.github.com>
2023-12-20 01:38:12 +0600

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,9 +1,9 @@
 def uppercase_string(text):
-    """docstring"""
+    """функция  заглавные буквы"""
     return text.upper()
 
 def capitalize_words(string):
-    """docstring"""
+    """делает заглавной буквой каждого слова"""
     words = string.split()  # Разделяем строку на список слов
     capitalized_words = [word.capitalize() for word in words]  # Делаем первую букву каждого слова заглавной
     result = " ".join(capitalized_words)  # Соединяем слова обратно в строку
